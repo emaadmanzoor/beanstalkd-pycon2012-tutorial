beec6855c8fecdd266cbcd1fddc6342301dc4a34
Emaad Ahmed Manzoor <emaadm@yahoo-inc.com>
2012-09-28 06:02:07 +0530

    corrected code indent

--- presentation/beanstalkd-pycon2012.rb
+++ presentation/beanstalkd-pycon2012.rb
@@ -321,20 +321,20 @@ section "Now, we code" do
         while True:
             print "Waiting for work..."
 
-        job = beanstalk.reserve()
+            job = beanstalk.reserve()
 
-        dartsToThrow = int(job.body)
-        dartsInCircle = 0
-        for i in range(dartsToThrow):
-            x = (random.random() * piconfig.SQEDGELEN) - (piconfig.SQEDGELEN/2)
-            y = (random.random() * piconfig.SQEDGELEN) - (piconfig.SQEDGELEN/2)
-            if x ** 2 + y ** 2 <= circleRadius ** 2:
-                dartsInCircle += 1
+            dartsToThrow = int(job.body)
+            dartsInCircle = 0
+            for i in range(dartsToThrow):
+                x = (random.random() * piconfig.SQEDGELEN) - (piconfig.SQEDGELEN/2)
+                y = (random.random() * piconfig.SQEDGELEN) - (piconfig.SQEDGELEN/2)
+                if x ** 2 + y ** 2 <= circleRadius ** 2:
+                    dartsInCircle += 1
 
-        beanstalk.put(str(dartsInCircle))
+            beanstalk.put(str(dartsInCircle))
 
-        print "Found", dartsInCircle, "out of", job.body, "darts within the circle."
-        job.delete()
+            print "Found", dartsInCircle, "out of", job.body, "darts within the circle."
+            job.delete()
     EOS
             
     slide <<-EOS, :center

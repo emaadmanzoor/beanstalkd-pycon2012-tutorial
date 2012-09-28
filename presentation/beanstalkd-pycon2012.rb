1e0ac02dae42f3e0e2c31773559a932deec2da98
Rachee <rachee.singh@gmail.com>
2012-09-28 07:48:31 +0530

    adding code for matrix mul worker

--- presentation/beanstalkd-pycon2012.rb
+++ presentation/beanstalkd-pycon2012.rb
@@ -511,6 +511,56 @@ section "Now, we code" do
             print ( mc.get( str( i*matconfig.MATRIX_SIZE + j ) ) ),
         print
   EOS
+
+  slide <<-EOS, :code
+     import beanstalkc
+     import memcache
+     import time
+     import operator
+     import matconfig
+ 
+     while True:                            # Indefinitely wait for jobs to do
+        print "Connecting to job server..."
+        beanstalk = beanstalkc.Connection( host='127.0.0.1', port=11300 )
+        beanstalk.watch( matconfig.JOBS )     # Reserve jobs from this queue
+        print "Waiting for jobs..."
+
+        job = beanstalk.reserve()           # Blocks until a job is available
+
+        print "Reserved a job.."
+        [ strI, strJ, stringRow, stringColumn ] = job.body.split( '\\n' )
+        I = int( strI )
+        J = int( strJ )
+        row = map( int, stringRow.split() )
+        column = map( int, stringColumn.split() )
+        
+        n = len( row )
+        start_time = time.time()
+        
+        print 'Computing Cij..'
+        try:
+            result = reduce( operator.add, map( operator.mul, row, column ) )
+        except Exception, e:
+            print e
+            print "Job failed.", n
+            job.release()
+
+        mc = memcache.Client ([ '127.0.0.1:11211' ], debug=0 )
+        print 'Wrote result to',  str( I+J )
+        mc.set( str( I + J ), result )
+        beanstalk.use( matconfig.ACKS )
+        beanstalk.put( strI + strJ )
+        
+        job.delete()                        # Tell the job server you're done
+        beanstalk.close()
+
+        print "Finished job in", str(time.time() - start_time), "seconds"
+        print
+  EOS
+
+end
+
+section "Let's test that" do
 end
 
 section "And we're done, thanks!" do
